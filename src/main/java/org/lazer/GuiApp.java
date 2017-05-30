b7c1cb0f7f372975dfe4b63c3f1825ca949b32fb
alrogado <7aloro74>
2017-05-30 16:37:12 +0200

    Deleted comments

--- src/main/java/org/lazer/GuiApp.java
+++ src/main/java/org/lazer/GuiApp.java
@@ -1,10 +1,6 @@
 package org.lazer;
 
 import com.jfoenix.controls.JFXDecorator;
-import com.jfoenix.svg.SVGGlyphLoader;
-import eu.hansolo.tilesfx.Tile;
-import eu.hansolo.tilesfx.TileBuilder;
-import eu.hansolo.tilesfx.tools.FlowGridPane;
 import io.datafx.controller.ViewConfiguration;
 import io.datafx.controller.flow.Flow;
 import io.datafx.controller.flow.FlowException;
@@ -13,28 +9,24 @@ import io.datafx.controller.flow.container.DefaultFlowContainer;
 import io.datafx.controller.flow.context.FXMLViewFlowContext;
 import io.datafx.controller.flow.context.ViewFlowContext;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.concurrent.Task;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.CycleMethod;
 import javafx.scene.paint.LinearGradient;
 import javafx.scene.paint.Stop;
-import javafx.scene.shape.Rectangle;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import org.fxmisc.cssfx.CSSFX;
-import org.reactfx.EventStreams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.time.Duration;
 import java.time.Instant;
 import java.util.Locale;
 import java.util.ResourceBundle;
@@ -46,24 +38,82 @@ public class GuiApp extends Application {
     @FXMLViewFlowContext
     private ViewFlowContext ViewFlowContext = null;
 
-    private              long            epochSeconds;
+    private long epochSeconds;
+
+    private Stage mainStage;
+
+    JFXDecorator decorator;
+    ResourceBundle bundle = ResourceBundle.getBundle("lazer", new Locale("fr"));
+
+    BooleanProperty ready = new SimpleBooleanProperty(false);
 
     public void start(Stage stage) {
-        epochSeconds = Instant.now().getEpochSecond();
-        /*new Thread(() -> {
-            try {
-                SVGGlyphLoader.loadGlyphsFont(getClass().getResourceAsStream("/fonts/icomoon.svg"),"icomoon.svg");
-            } catch (IOException e) {
-                e.printStackTrace();
+        logger.debug("start");
+        mainStage = stage;
+
+        configureInitController(configureDataFlow(GuiAppController.class));
+
+        loadSplashAndInitTask();
+
+        ready.addListener((ObservableValue<? extends Boolean> ov, Boolean t, Boolean t1) -> {
+            if (Boolean.TRUE.equals(t1)) {
+                Platform.runLater(() -> {
+                    logger.debug("Init Task  completed. Now executes showMainStage");
+                    showMainStage();
+                });
             }
-        }).start();*/
+        });
 
+        epochSeconds = Instant.now().getEpochSecond();
+    }
+
+    private void showMainStage() {
+
+        Stop[] stops = new Stop[] { new Stop(0, Color.BLACK), new Stop(1, Color.RED)};
+        LinearGradient lg1 = new LinearGradient(0, 0, 1, 0, true, CycleMethod.NO_CYCLE, stops);
+
+        int width = 800;
+        int height = 850;
+        Scene scene = new Scene(decorator, width, height);
+        //Scene scene = new Scene(new StackPane(label = new Label()), 800, 850);
+        final ObservableList<String> stylesheets = scene.getStylesheets();
+        stylesheets.addAll(getClass().getResource("/css/jfoenix-fonts.css").toExternalForm(),
+                getClass().getResource("/css/jfoenix-design.css").toExternalForm(),
+                getClass().getResource("/org/lazer/css/jfoenix-components.css").toExternalForm(),
+                getClass().getResource("/org/lazer/css/jfoenix-main-demo.css").toExternalForm());
+
+        //mainStage.setMaxWidth(400.0);
+
+        /*Rectangle r1 = new Rectangle(0, 0, 100, 100);
+        r1.setFill(lg1);*/
+
+        scene.setFill(lg1);
+
+        //mainStage.setFullScreen(true);
+        //mainStage.setAlwaysOnTop(true);
+        final Rectangle2D bounds = Screen.getPrimary().getBounds();
+        mainStage.setX(bounds.getMinX() + bounds.getWidth() / 2 - width / 2);
+        mainStage.setY(bounds.getMinY() + bounds.getHeight() / 2 - height/ 2);
+        mainStage.setMinHeight(height);
+        mainStage.setMinWidth(width);
+        mainStage.setHeight(height);
+        mainStage.setWidth(width);
+        mainStage.setScene(scene);
+        mainStage.setTitle("LAZER App");
+        //mainStage.initStyle(StageStyle.UNDECORATED);
+        mainStage.setFullScreenExitHint("");
+        mainStage.show();
+
+        CSSFX.start(mainStage);
+    }
+
+    private DefaultFlowContainer configureDataFlow(Class<GuiAppController> startViewControllerClass) {
         ViewConfiguration viewConfiguration = new ViewConfiguration();
-        viewConfiguration.setResources(ResourceBundle.getBundle("lazer", new Locale("fr")));
+        viewConfiguration.setResources(bundle);
 
-        Flow flow = new Flow(GuiAppController.class, viewConfiguration);
+        Flow flow = new Flow(startViewControllerClass, viewConfiguration);
         ViewFlowContext flowContext = new ViewFlowContext();
-        flowContext.register("Stage", stage);
+        flowContext.register("Stage", mainStage);
 
         DefaultFlowContainer container = new DefaultFlowContainer();
         FlowHandler handler = new FlowHandler(flow, flowContext, viewConfiguration);
@@ -72,58 +122,46 @@ public class GuiApp extends Application {
         } catch (FlowException e) {
             logger.error("",e);
         }
+        return container;
+    }
 
-        JFXDecorator decorator = new JFXDecorator(stage, container.getView());
-        decorator.setCustomMaximize(true);
-        Stop[] stops = new Stop[] { new Stop(0, Color.BLACK), new Stop(1, Color.RED)};
-        LinearGradient lg1 = new LinearGradient(0, 0, 1, 0, true, CycleMethod.NO_CYCLE, stops);
-
-
-        Scene scene = new Scene(decorator);
-        //Scene scene = new Scene(new StackPane(label = new Label()), 800, 850);
-        final ObservableList<String> stylesheets = scene.getStylesheets();
-        stylesheets.addAll(getClass().getResource("/css/jfoenix-fonts.css").toExternalForm(),
-                getClass().getResource("/css/jfoenix-design.css").toExternalForm(),
-                getClass().getResource("/org/lazer/css/jfoenix-components.css").toExternalForm(),
-                getClass().getResource("/org/lazer/css/jfoenix-main-demo.css").toExternalForm());
-        //stage.setMaxWidth(400.0);
-
-        /*Rectangle r1 = new Rectangle(0, 0, 100, 100);
-        r1.setFill(lg1);*/
-
-        scene.setFill(lg1);
-
-
-        stage.setMinHeight(400.0);
-        stage.setMinWidth(400.0);
-        stage.setScene(scene);
-        stage.setTitle("LAZER App");
-        stage.initStyle(StageStyle.UNDECORATED);
-        //stage.setFullScreen(true);
-        //stage.setAlwaysOnTop(true);
-        stage.setFullScreenExitHint("");
-        stage.show();
-        CSSFX.start(stage);
-
-        //ObservableExecutor.getDefaultInstance().createProcessChain().addRunnableInPlatformThread(() -> label.setText(new Date() + "")).repeatInfinite(Duration.seconds(1));
-
-
-        /*FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setResources(ResourceBundle.getBundle("lazer", new Locale("fr")));
-        Parent root = null;
+    private void loadSplashAndInitTask() {
+        Preloader preloaderFX = new Preloader();
         try {
-            root = fxmlLoader.load(getClass().getResourceAsStream("fxml/lazer.fxml"));
-        }catch(NullPointerException npe){
-            System.out.println("No se ha encontrado el fichero con la aplicación. "+ npe.getLocalizedMessage());
+            preloaderFX.start(mainStage);
+        } catch (Exception e) {
+            logger.error("Problem launching preloader",e);
         }
-        //borderPane.setCenter(pane);
+        preloaderFX.handleProgressNotification(new javafx.application.Preloader.ProgressNotification(0));
 
-        //primaryStage.setTitle("Hello World");
-        stage.setFullScreen(true);
-        stage.setAlwaysOnTop(true);
-        stage.setFullScreenExitHint("");
-        stage.setScene(new Scene(root, 350, 475));
-        stage.show();*/
+        //simulate long init in background
+        Task task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                int max = 3;
+                for (int i = 1; i <= max; i++) {
+                    Thread.sleep(500);
+                    logger.debug("loadSplashAndInitTask " + i);
+                    // Send progress to preloader
+                    preloaderFX.handleProgressNotification(new javafx.application.Preloader.ProgressNotification(((double) i)/max)); //this moves the progress bar of the preloader
+                }
+                ///Alwways close the dialog because the main can't be closed
+                preloaderFX.stop();
+                Thread.sleep(500);
+                // After init is ready, the app is ready to be shown
+                // Do this before hiding the preloader stage to prevent the
+                // app from exiting prematurely
+                ready.setValue(Boolean.TRUE);
+                //preloaderFX.handleStateChangeNotification(new Preloader.StateChangeNotification(Preloader.StateChangeNotification.Type.BEFORE_START));
+                return null;
+            }
+        };
+        new Thread(task).start();
+    }
+
+    private void configureInitController(DefaultFlowContainer container) {
+        decorator = new JFXDecorator(mainStage, container.getView());
+        decorator.setCustomMaximize(true);
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/org/lazer/Preloader.java
@@ -0,0 +1,144 @@
+package org.lazer;
+
+import com.jfoenix.controls.JFXDialog;
+import io.datafx.controller.ViewController;
+import javafx.beans.property.DoubleProperty;
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by alrogado on 5/30/17.
+ */
+public class Preloader extends javafx.application.Preloader {
+
+    private static Logger logger = LoggerFactory.getLogger(Preloader.class);
+
+    public static final String APPLICATION_ICON = "http://cdn1.iconfinder.com/data/icons/Copenhagen/PNG/32/people.png";
+
+    public static final String SPLASH_IMAGE = "images/logov.png";
+
+    boolean noLoadingProgress = true;
+
+    private StackPane stackPane;
+    private JFXDialog dialog;
+    private Pane splashLayout;
+    private ProgressBar loadProgress;
+    private Label progressText;
+    private ImageView splash;
+
+    private static final int SPLASH_WIDTH = 275;
+    private static final int SPLASH_HEIGHT = 183;
+
+    public Preloader() {
+        init();
+    }
+
+    @Override
+    public void init() {
+        splash = new ImageView(new Image(SPLASH_IMAGE,SPLASH_WIDTH,SPLASH_HEIGHT,false,false));
+        loadProgress = new ProgressBar();
+        loadProgress.setPrefWidth(SPLASH_WIDTH);
+        progressText = new Label("%menu.title.cut");
+        splashLayout = new VBox();
+        splashLayout.getChildren().addAll(splash, loadProgress, progressText);
+        progressText.setAlignment(Pos.CENTER);
+        splashLayout.setStyle(
+                "-fx-padding: 10; "
+                        + "-fx-background-color: white; "
+                        + "-fx-border-width:5; "
+        );
+        splashLayout.setEffect(new DropShadow());
+        stackPane = new StackPane();
+        dialog = new JFXDialog(stackPane, splashLayout, JFXDialog.DialogTransition.CENTER);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        logger.debug("PreloaderFx::start();");
+        //stage.setTitle("Progress Bar");
+        //stage.getIcons().add(new Image(APPLICATION_ICON));
+        final Rectangle2D bounds = Screen.getPrimary().getBounds();
+        stage.setScene(new Scene(stackPane));
+        stage.setX(bounds.getMinX() + bounds.getWidth() / 2 - SPLASH_WIDTH / 2);
+        stage.setY(bounds.getMinY() + bounds.getHeight() / 2 - SPLASH_HEIGHT / 2);
+        stage.show();
+        dialog.show();
+    }
+
+    @Override
+    public void handleStateChangeNotification(StateChangeNotification scn) {
+        if (scn.getType() == StateChangeNotification.Type.BEFORE_START) {
+            dialog.close();
+        }
+    }
+
+    /*@Override
+    public void handleProgressNotification(ProgressNotification pn) {
+        loadProgress.setProgress(pn.getProgress());
+    }*/
+
+    public void setProgress(double d) {
+        loadProgress.setProgress(d);
+    }
+
+    public DoubleProperty getProgress() {
+        return loadProgress.progressProperty();
+    }
+
+    @Override
+    public void handleProgressNotification(ProgressNotification pn) {
+        logger.debug("PreloaderFx::handleProgressNotification(); progress = " + pn.getProgress());
+        //Even if there is nothing to load 0% and 100% events can be delivered
+        if (pn.getProgress() != 1.0 || !noLoadingProgress) {
+            loadProgress.setProgress(pn.getProgress());
+            if (pn.getProgress() > 0) {
+                noLoadingProgress = false;
+            }
+        }
+    }
+
+    /*@Override
+    public void handleStateChangeNotification(StateChangeNotification evt) {
+        //ignore, hide after application signals it is ready
+        logger.debug("PreloaderFx::handleStateChangeNotification(); state = " + evt.getType());
+    }*/
+
+    @Override
+    public void handleApplicationNotification(PreloaderNotification pn) {
+        if (pn instanceof ProgressNotification) {
+            //expect application to send us progress notifications
+            //with progress ranging from 0 to 1.0
+            double v = ((javafx.application.Preloader.ProgressNotification) pn).getProgress();
+            logger.debug("PreloaderFx::handleApplicationNotification(); progress = " + v);
+            //if (!noLoadingProgress) {
+            //if we were receiving loading progress notifications
+            //then progress is already at 50%.
+            //Rescale application progress to start from 50%
+            v = 0.5 + v / 2;
+            //}
+            loadProgress.setProgress(v);
+        } else if (pn instanceof StateChangeNotification) {
+            logger.debug("PreloaderFx::handleApplicationNotification(); state = " + ((StateChangeNotification) pn).getType());
+            //hide after get any state update from application
+            dialog.close();
+        }
+    }
+
+    public void stop() {
+        dialog.close();
+    }
+}
