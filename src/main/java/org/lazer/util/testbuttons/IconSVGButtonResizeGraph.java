2c0077914273cf23c725952d02cec0c8ad7b5131
alrogado <7aloro74>
2017-06-14 08:58:32 +0200

    using icon button class

--- src/main/java/org/lazer/util/testbuttons/IconSVGButtonResizeGraph.java
+++ src/main/java/org/lazer/util/testbuttons/IconSVGButtonResizeGraph.java
@@ -11,6 +11,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.shape.SVGPath;
 import javafx.stage.Stage;
+import org.fxmisc.cssfx.CSSFX;
 import org.kordamp.ikonli.fontelico.Fontelico;
 import org.kordamp.ikonli.javafx.FontIcon;
 import org.lazer.GuiApp;
@@ -27,46 +28,33 @@ public class IconSVGButtonResizeGraph extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage stage) throws Exception {
 
-        HBox root = new HBox();
-        root.setAlignment(Pos.CENTER);
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER);
 
-
-        Button btn = new JFXButton();
+        Button btn = new Button();
         btn.getStyleClass().add("icon-button");
         btn.setPickOnBounds(true);
 
         Region icon = new Region();
         icon.getStyleClass().add("icon");
         btn.setGraphic(icon);
-        btn.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
 
-
-        //buttonWithGraphics.setGraphic(fontIcon);
-
-        // Bind the Image scale property to the buttons size
-        icon.scaleXProperty().bind(btn.widthProperty().divide(100));
-        icon.scaleYProperty().bind(btn.heightProperty().divide(100));
-
-
-        // Declare a minimum size for the button
-        btn.setMinSize(MIN_BUTTON_SIZE, MIN_BUTTON_SIZE);
-
-        root.getChildren().addAll(btn);
-        root.layoutBoundsProperty().addListener((observableValue, oldBounds, newBounds) -> {
+        hBox.getChildren().addAll(btn);
+        hBox.layoutBoundsProperty().addListener((observableValue, oldBounds, newBounds) -> {
                     double size = Math.max(MIN_BUTTON_SIZE, Math.min(newBounds.getWidth(), newBounds.getHeight()));
                     btn.setPrefSize(size, size);
                 }
         );
 
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(hBox);
+        scene.getStylesheets().addAll(GuiApp.class.getResource("/org/lazer/css/button-icon.css").toExternalForm());
 
-        final ObservableList<String> stylesheets = scene.getStylesheets();
-        stylesheets.addAll(GuiApp.class.getResource("/org/lazer/css/button-icon.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
 
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        CSSFX.start(stage);
     }
 
 
