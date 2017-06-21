85dcd4d1fa01ba8d6dcd1007580ba19ee267ed68
alrogado <7aloro74>
2017-06-21 14:41:55 +0200

    Ajustes en Main Controller

--- src/main/java/org/testjfx/controllers/MainController.java
+++ src/main/java/org/testjfx/controllers/MainController.java
@@ -40,35 +40,19 @@ public class MainController {
 
     @FXMLViewFlowContext
     private ViewFlowContext context;
+
     @FXML
     private StackPane root;
-    @FXML
-    private StackPane titleBurgerContainer;
-    @FXML
-    private JFXHamburger titleBurger;
-    @FXML
-    private StackPane optionsBurger;
     @FXML
     private JFXRippler settingsRippler;
     @FXML
     private JFXRippler houseRippler;
     @FXML
     private JFXToolbar toolbar;
-
     @FXML
     private JFXDrawer drawer;
 
-    private JFXPopup toolbarPopup;
-    @FXML
-    private Label labelChecks;
-    @FXML
-    private Label labelMainContent;
-
-
-
-    public MainController() {
-        this.labelMainContent=this.labelMainContent;
-    }
+    Class startViewControllerClass = RegulatorsController.class;
 
     /**
      * init fxml when loaded.
@@ -80,64 +64,24 @@ public class MainController {
         // set the default controller
         ViewConfiguration viewConfiguration = new ViewConfiguration();
         viewConfiguration.setResources(Configuration.APPBUNDLE);
-        Flow innerFlow = new Flow(RegulatorsController.class, viewConfiguration);
+        Flow innerFlow = new Flow(startViewControllerClass, viewConfiguration);
         FlowHandler flowHandler = innerFlow.createHandler(context);
         context.register("ContentFlowHandler", flowHandler);
         context.register("ContentFlow", innerFlow);
         drawer.setContent(flowHandler.start(new ExtendedAnimatedFlowContainer(ANIM_DURATION, SWIPE_LEFT)));
         context.register("ContentPane", drawer.getContent().get(0));
-        //configureContent(MainContentController.class, drawer);
 
-        // side controller will add links to the content flow
-       /* Flow sideMenuFlow = new Flow(SideMenuController.class, viewConfiguration);
-        final FlowHandler sideMenuFlowHandler = sideMenuFlow.createHandler(context);
-        drawer.setSidePane(sideMenuFlowHandler.start(new ExtendedAnimatedFlowContainer(Duration.millis(320),SWIPE_LEFT)));
-        */
-
-        /*// init the title hamburger icon
-        drawer.setOnDrawerOpening(e -> {
-            final Transition animation = titleBurger.getAnimation();
-            animation.setRate(1);
-            animation.play();
-        });
-        drawer.setOnDrawerClosing(e -> {
-            final Transition animation = titleBurger.getAnimation();
-            animation.setRate(-1);
-            animation.play();
-        });*/
-
-        /*FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/testjfx/fxml/ui/main_popup.fxml"));
-        loader.setController(new InputController(this));
-        loader.setResources(APPBUNDLE);
-        try {
-            toolbarPopup = new JFXPopup(loader.load());
-        } catch (IOException e) {
-            logger.error("",e);
-        }
-
-        optionsBurger.setOnMouseClicked(e -> toolbarPopup.show(optionsBurger,
-                JFXPopup.PopupVPosition.TOP,
-                JFXPopup.PopupHPosition.RIGHT,
-                -12,
-                15));*/
-
-
-        /*bindNodeToController(labelChecks, TwoTilesTilesController.class, innerFlow, flowHandler);
-        bindNodeToController(labelMainContent, MainContentController.class, innerFlow, flowHandler);*/
+        houseRippler.setControl(customizeIkon(Typicons.HOME_OUTLINE));
+        houseRippler.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> mouseEventFlow(event, flowHandler, startViewControllerClass));
 
         settingsRippler.setControl(customizeIkon(MaterialDesign.MDI_ACCOUNT_SETTINGS_VARIANT ));
-        settingsRippler.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> mouseEventFlow(event, flowHandler, labelChecks, SettingsController.class));
-        houseRippler.setControl(customizeIkon(Typicons.HOME_OUTLINE));
-        houseRippler.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> mouseEventFlow(event, flowHandler, labelChecks, RegulatorsController.class));
+        settingsRippler.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> mouseEventFlow(event, flowHandler, SettingsController.class));
+
         toolbar.setCenter(ClockBuilder.createClock());
-
-        /*labelChecks.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> mouseEventFlow(event, flowHandler, labelChecks, SettingsController.class));
-        labelMainContent.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> mouseEventFlow(event, flowHandler, labelChecks, RegulatorsController.class));*/
-
 
     }
 
-    private void mouseEventFlow(MouseEvent event, FlowHandler flowHandler, Node node, Class controllerClass) {
+    private void mouseEventFlow(MouseEvent event, FlowHandler flowHandler,  Class controllerClass) {
         if (event.getClickCount() == 1) {
             try {
                 //flowHandler.handle(node.getId());
@@ -150,19 +94,7 @@ public class MainController {
         }
     }
 
-    /*@FXML
-    private void handleMousePress(MouseEvent event) {
-        // code in this method is executed when the mouse is pressed
-        // on a node with onMousePressed="#handleMousePress"
-        //FlowHandler flowHandler = innerFlow.createHandler(context);
-        try {
-            flowHandler.handle(labelChecks.getId());
-        } catch (VetoException e) {
-            e.printStackTrace();
-        } catch (FlowException e) {
-            e.printStackTrace();
-        }
-    }*/
+
 
     private void bindNodeToController(Node node, Class<?> controllerClass, Flow flow, FlowHandler flowHandler) {
         //flowHandler.getFlowContext().getCurrentViewContext().getConfiguration().getBuilderFactory().getBuilder()
@@ -170,49 +102,4 @@ public class MainController {
         flow.withGlobalLink(node.getId(), controllerClass);
     }
 
-    /*public static final class InputController {
-        @FXML
-        private JFXListView<?> toolbarPopupList;
-
-        MainController mainController;
-
-        public InputController(MainController mainController) {
-            this.mainController = mainController;
-        }
-
-        // close application
-        @FXML
-        private void submit() {
-            if (toolbarPopupList.getSelectionModel().getSelectedIndex() == 2) {
-                Platform.exit();
-            }else if (toolbarPopupList.getSelectionModel().getSelectedIndex() == 1) {
-                try {
-                    initController(Sprite3dController.class);
-                } catch (FlowException e) {
-                    logger.error("",e);
-                }
-            }else if (toolbarPopupList.getSelectionModel().getSelectedIndex() == 0) {
-                try {
-                    //configureContent(MainContentController.class, mainController.drawer);
-                    //esto hace lo mismo y no hace el swipe en el content del drawer como sí lo hace el menu de la izq
-                    initController(MainContentController.class);
-                } catch (FlowException e) {
-                    logger.error("",e);
-                }
-            }
-        }
-
-        private void initController(Class controllerClass) throws FlowException {
-            Flow innerFlow = new Flow(controllerClass);
-            final FlowHandler flowHandler = innerFlow.createHandler(mainController.context);
-            if(((FlowHandler) mainController.context.getRegisteredObject("ContentFlowHandler")).getCurrentViewContext().getController() instanceof EffectRunnable){
-                ((AbstractEffectsController)((FlowHandler) mainController.context.getRegisteredObject("ContentFlowHandler")).getCurrentViewContext().getController()).stopEffects();
-            }
-            mainController.context.register("ContentFlowHandler", flowHandler);
-            mainController.context.register("ContentFlow", innerFlow);
-            final Duration containerAnimationDuration = Duration.millis(320);
-            mainController.drawer.setContent(flowHandler.start(new ExtendedAnimatedFlowContainer(containerAnimationDuration, SWIPE_LEFT)));
-            mainController.context.register("ContentPane", mainController.drawer.getContent().get(0));
-        }
-    }*/
 }
